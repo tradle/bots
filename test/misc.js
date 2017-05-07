f9025b6f115e15e94399040f6cb20c049cc10c14
Mark Vayngrib <mark@tradle.io>
2017-05-07 12:11:56 -0400

    factor out lib/locker to promise-locker module

--- test/misc.js
+++ test/misc.js
@@ -8,7 +8,6 @@ const {
   wait
 } = require('../lib/utils')
 
-const createLocker = require('../lib/locker')
 const createStore = require('../lib/store')
 const cachify = require('../lib/cachify')
 const PromiseEmitter = require('../lib/promise-emitter')
@@ -108,39 +107,6 @@ test('bubble', co(function* (t) {
 
   t.equal(ran, 2)
   t.end()
-}))
-
-test('lock', co(function* (t) {
-  const lock = createLocker()
-  const unlockA = yield lock('a')
-  let unlockedA
-  lock('a')
-    .then(() => {
-      t.equal(unlockedA, true)
-      t.end()
-    })
-    .catch(t.error)
-
-  // can lock on another id
-  const unlockB = yield lock('b')
-  yield wait(100)
-  unlockedA = true
-  unlockA()
-}))
-
-test('lock timeout', co(function* (t) {
-  // prevent exit
-  const timeout = setTimeout(() => {}, 1000)
-
-  const lock = createLocker({ timeout: 100 })
-  yield lock('a')
-  const start = Date.now()
-  yield lock('a')
-  const time = Date.now() - start
-  t.ok(time > 50)
-  t.end()
-
-  clearTimeout(timeout)
 }))
 
 test('promise emitter', co(function* (t) {
